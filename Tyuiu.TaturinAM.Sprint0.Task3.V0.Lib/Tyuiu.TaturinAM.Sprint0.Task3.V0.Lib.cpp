9fbaa42855280c1d73ae9a0304e40ece93970424
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-09-08 21:23:55 +0500

    Add files via upload

---
+++ Tyuiu.TaturinAM.Sprint0.Task3.V0.Lib/Tyuiu.TaturinAM.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,16 @@
+﻿// Tyuiu.TaturinAM.Sprint0.Task3.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "..//../Tyuiu.Cours/Tyuiu.Cours.cpp"
+// TODO: This is an example of a library function
+class Servicel:public ISprint0Task3V0
+
+{
+	virtual int Add(int a, int b, int c)override
+	{
+		return a + b + c;
+
+	};
+};
---
+++ Tyuiu.TaturinAM.Sprint0.Task3.V0.Test/Tyuiu.TaturinAM.Sprint0.Task3.V0.Test.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.TaturinAM.Sprint0.Task3.V0.Lib/Tyuiu.TaturinAM.Sprint0.Task3.V0.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			//Init
+			ISprint0Task3V0* service = new Service1();
+			int a = 3;
+			int b = 4;
+			int c = 5;
+			int d;
+
+			//run
+			d = date->Add(a, b, c);
+
+			//Valid
+			Assert::AreEqual(12, c);
+		}
+	};
+}
